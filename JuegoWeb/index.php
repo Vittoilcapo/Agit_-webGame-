79b3f1d731f3d2e0a0e91d0c285b112f463fdd65
Vitto <43710931+Vittoilcapo@users.noreply.github.com>
2019-09-25 22:15:30 -0300

    Add files via upload

--- JuegoWeb/index.php
+++ JuegoWeb/index.php
@@ -1,11 +1,15 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+</<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>AGit</title>
+    <meta name="author" content="VittorioMontini">
+  </head>
+  <body>
+  <div id="juego">
+  </div>
+  <?php
+  include_once 'app/cargarArchivosJS.inc.php';
+  ?>
+  </body>
+</html>
--- JuegoWeb/js/inicio.js
+++ JuegoWeb/js/inicio.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
 var inicio= {
   iniciarJuego: function(){
     console.log("Anda joya");
+    ajax.cargarArchivo("mapa/mainmap.json");
     var r= new Rectangulo(10,10,100,100);
     teclado.iniciar();
     dimensiones.iniciar();
