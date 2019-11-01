1937633e6f81804bcffd0fda126ca25e556b61cd
Vitto <43710931+Vittoilcapo@users.noreply.github.com>
2019-11-01 13:41:16 -0300

    Borre la carpeta de Juego web porque no iba
    Co-Authored-By: Vitto <Vittoilcapo@users.noreply.github.com>

--- JuegoWeb/js/Sprite.js
+++
@@ -1,6 +0,0 @@
-function Sprite(ruta, idSobreZero, posicionEnHoja) {
-	this.rutaHojaOrigen = "mapa/beastlands.tsx"
-	this.idSobreZero = idSobreZero;
-	this.idSobreUno = idSobreZero + 1;
-	this.posicionEnHoja = posicionEnHoja;
-}
