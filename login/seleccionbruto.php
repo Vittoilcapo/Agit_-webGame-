a0789db7807e76070d2d2b5566fa057d5da319d5
foloxx <fede10320@gmail.com>
2019-11-06 22:08:05 -0300

    Ya se guarda el lcuhador seleccionado. Falta conexion sql.
    Co-Authored-By: null <fvigliarolo@users.noreply.github.com>

---
+++ login/seleccionbruto.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/seleccionbruto.css">
+  <head>
+    <title>Seleccionar campeones</title>
+
+<form method="get" action="pruebabruto.php"
+<article>
+    <div id="boxeador">
+          <img src="img\boxeador.png" width="250" height="200" alt="foto boxeador"/>
+          <br>
+          <input id="botonboxeador"  type="submit"  name="boxeadorcomprar" value="comprar">
+
+                </div>
+
+
+
+    <div id="caballero">
+          <img src="img\caballero.gif" width="250" height="200" alt="pacote"/>
+          <br>
+          <input id="botoncaballero"  type="submit"  name="caballerocomprar" value="comprar">
+
+                </div>
+
+
+
+    <div id="raccoon">
+          <img src="img\raccoon.gif" width="250" height="200" alt="pacote"/>
+          <br>
+          <input id="botonraccoon"  type="submit"  name="raccooncomprar" value="comprar">
+
+                </div>
+
+
+</article>
+
+
+
+
+
+  </head>
+  <body>
+
+  </body>
+</html>
