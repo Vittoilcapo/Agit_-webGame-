36fa4fdc3fad57ddbf1f4139ce89f50b5246f96a
foloxx <fede10320@gmail.com>
2019-11-23 11:00:42 -0300

    comienzo a hacer la tienda de skin
    Co-Authored-By: null <fvigliarolo@users.noreply.github.com>

---
+++ Tienda/comprarskin.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/seleccionbruto.css">
+  <head>
+    <title>Seleccionar campeones</title>
+
+<form method="get" action="pruebabruto.php">
+
+
+
+<br>
+<br>
+<article>
+    <div id="boxeador">
+          <img src="img\boxeador.png" width="250" height="200" alt="foto boxeador"/>
+          <br>
+          <input id="botonboxeador"  type="submit"  name="boxeadorcomprar" value="seleccionar">
+
+                </div>
+
+
+
+    <div id="caballero">
+          <img src="img\caballero.gif" width="250" height="200" alt="pacote"/>
+          <br>
+          <input id="botoncaballero"  type="submit"  name="caballerocomprar" value="seleccionar">
+
+                </div>
+
+
+
+    <div id="raccoon">
+          <img src="img\raccoon.gif" width="250" height="200" alt="pacote"/>
+          <br>
+          <input id="botonraccoon"  type="submit"  name="raccooncomprar" value="seleccionar">
+
+                </div>
+
+
+</article>
+
+
+
+
+
+
+
+  </head>
+  <body>
+
+  </body>
+</html>
