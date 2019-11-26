ce6868012556a5539956ad1ffdcb7e584645aea0
Vitto <43710931+Vittoilcapo@users.noreply.github.com>
2019-11-25 23:05:09 -0300

    Ya carga a los rivales de la BD
    Co-Authored-By: Vitto <Vittoilcapo@users.noreply.github.com>

--- Partida/elegirPeleador.php
+++ Partida/elegirPeleador.php
@@ -1,7 +1,6 @@
 <?php
-include_once('../Tienda(connectBD.php)');
-
- ?>
+include_once('../Tienda/connectBD.php');
+?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -9,20 +8,38 @@ include_once('../Tienda(connectBD.php)');
     <link rel="stylesheet" href="estilo_pelea.css">
   </head>
     <header>
-      <h1>Eliga contra quien quiere pelear</h1>
+      <h1 style="text-align: center">Eliga contra quien quiere pelear</h1>
   </header>
   <body>
 
     <div class="contenedor">
+      <?php
+        $sql ="select u.usuario_id, u.usuario_nombre, u.usuario_nivel, i.inventario_skin_foto, e.estadisticas_velocidad, e.estadisticas_fuerza, e.estadisticas_agilidad from usuario u inner join inventario_skin i inner join estadisticas e
+              where u.inventario_id = i.inventario_id and e.usuario_mail = u.usuario_mail;"; // obtengo a los rivales disponibles
+              /*
+                FALTA QUE SOLO OBTENGA A LOS QUE SON PARECIDOS A SU NIVEL
+              */
+        $resultado = conectar($sql);
+        $numero = mysqli_num_rows($resultado);
 
+        while ($result = mysqli_fetch_array($resultado)){
+          $Armas[] = $result["usuario_id"];
+          echo "<div class='rivales'><h4>".$result["usuario_nombre"]."</h4> <h3>".$result["usuario_nivel"]."</h3><img src=".$result["inventario_skin_foto"]."></div>";
+
+        }
+
+      ?>
     </div>
 
     <form class= "skins" method="post" action="elegirSkin.php" >
     <div class="checkboxes">
-
-
+      <?php
+      for ($i = 0; $i < count($Armas); $i++) {
+      echo "<div class ='check'> <input type='radio' name='rivales' value=".$Armas[$i]."></div>";
+      }
+      ?>
     </div>
-    <div class= "comprar" style="text-align: center"><input type="submit" name="comprar" value="Pelear"></div>
+    <div class= "btnPelar" style="text-align: center"><input type="submit" name="Pelear" value="Pelear"></div>
     </form>
 
   </body>
