8fe18166dd6cf9a787fa5464b414347fe125b706
foloxx <fede10320@gmail.com>
2019-11-29 16:04:57 -0300

    va el wea
    Co-Authored-By: null <fvigliarolo@users.noreply.github.com>

--- Tienda/wea.php
+++ Tienda/wea.php
@@ -1,3 +1,5 @@
+
+
 <?php
 session_start();
 include_once('connectBD.php');
@@ -7,7 +9,8 @@ $nombre_arma = $_GET['inventarioNombre'];
 $id_inventario = $_SESSION['id_inventario'];
 
 
-    $Sentencia_sql="update inventario_armas set armasDefault = '1' where (inventario_id =".$id_inventario.") and (inventario_armas_nombre = '".$nombre_arma."');";
+    $Sentencia_sql="update inventario_armas set armasDefault =1 where (inventario_id =".$id_inventario.") and (inventario_armas_nombre = '".$nombre_arma."');";
+    //$Sentencia_sql="update `juego`.`inventario_armas` SET `armasdefault` =0 WHERE (`inventario_id` = '.$id_inventario.') and (`armasDefault` =1);";
     $resultado = conectar($Sentencia_sql);
     echo "cambiado con exito";
 
