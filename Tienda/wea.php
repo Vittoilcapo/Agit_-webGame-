042531fcce199a324781bcd420b7a787ab5872a3
jlumaca <lumacajuanmanuel@gmail.com>
2019-11-29 16:13:27 -0300

    dos update
    Co-Authored-By: null <jlumaca@users.noreply.github.com>

--- Tienda/wea.php
+++ Tienda/wea.php
@@ -9,9 +9,10 @@ $nombre_arma = $_GET['inventarioNombre'];
 $id_inventario = $_SESSION['id_inventario'];
 
 
-    $Sentencia_sql="update inventario_armas set armasDefault =1 where (inventario_id =".$id_inventario.") and (inventario_armas_nombre = '".$nombre_arma."');";
-    //$Sentencia_sql="update `juego`.`inventario_armas` SET `armasdefault` =0 WHERE (`inventario_id` = '.$id_inventario.') and (`armasDefault` =1);";
-    $resultado = conectar($Sentencia_sql);
+    $Sentencia_sql="update inventario_armas set armasDefault=1 where inventario_id =".$id_inventario." and inventario_armas_nombre = '".$nombre_arma."';";
+    $Sentencia_sql2="update inventario_armas SET armasDefault=0 WHERE inventario_id =" .$id_inventario. " and armasDefault=1;";
+    conectar($Sentencia_sql);
+        conectar($Sentencia_sql);
     echo "cambiado con exito";
 
 
