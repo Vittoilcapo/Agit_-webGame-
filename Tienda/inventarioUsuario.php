134cad65cf10d76bb5f64c9c3112a25ca71c9feb
jlumaca <lumacajuanmanuel@gmail.com>
2019-11-30 15:52:58 -0300

    ya agrega armadefault
    Co-Authored-By: null <jlumaca@users.noreply.github.com>

--- Tienda/inventarioUsuario.php
+++ Tienda/inventarioUsuario.php
@@ -1,4 +1,4 @@
-
+  <html>
 <?php
 include_once("connectBD.php");
 
@@ -12,47 +12,63 @@ class InventarioArmas{
 
 
   function obtenerInventario($id_inventario){
-   $Sentencia_sql="select i.inventario_armas_nombre, i.inventario_armas_velocidad, i.inventario_armas_fuerza, i.inventario_armas_agilidad, t.armas_imagen from
-   inventario_armas i join armas_tienda t where i.inventario_id =".$id_inventario." and t.armas_tienda_nombre = i.inventario_armas_nombre ;";
+   $Sentencia_sql="select i.inventario_id as id,i.inventario_armas_nombre as nombre, i.inventario_armas_velocidad as velocidad, i.inventario_armas_fuerza as fuerza, i.inventario_armas_agilidad as agilidad, t.armas_imagen as imagen
+   from inventario_armas i, armas_tienda t where i.inventario_id=".$id_inventario." and t.armas_tienda_nombre=i.inventario_armas_nombre ;";
 
    $resultado = conectar($Sentencia_sql);
 
 
     ?>
+
     <link rel="stylesheet" href="estilo_Inventario.css">
+    <body>
     <table class="grid-itemEstadisticas">
     <tr>
       <th>Nombre</th>
       <th>Velocidad</th>
       <th>Fuerza</th>
       <th>Agilidad</th>
-      <th>ArmaDefault</th>
+      <th>Arma por defecto</th>
     </tr>
 
     <?php
+
     while ($inventario = mysqli_fetch_array($resultado)){?>
       <div class="Armas">
-        <div class = "imagenesArmas"> <img src="<?php echo $inventario["armas_imagen"] ?>"> </div>
+        <div class = "imagenesArmas"> <img src="<?php echo $inventario["imagen"]; ?>"> </div>
       </div>
     <?php
+    $id=$inventario["id"];
     echo "<tr>";
+    /*
       echo "<td>".$inventario["inventario_armas_nombre"]."</td>
       <td>".$inventario["inventario_armas_velocidad"]. "</td>
       <td>".$inventario["inventario_armas_fuerza"]."</td>
       <td>".$inventario["inventario_armas_agilidad"]."</td>";
-      echo "<td>";
+*/
 
-        echo "<input type='radio' name='armasDefault' id='checkboxes' onclick=foo('".$inventario["inventario_armas_nombre"]."') >";
+echo "<td>"; echo $inventario["nombre"]; echo "</td>";
+echo "<td>"; echo $inventario["velocidad"]; echo "</td>";
+echo "<td>"; echo $inventario["fuerza"]; echo "</td>";
+echo "<td>"; echo $inventario["agilidad"]; echo "</td>";
+
+    //    echo "<input type='radio' name='armasDefault' id='checkboxes' onClick=foo('".$inventario["inventario_armas_nombre"]."') >";
+
+        echo "<td><a href='wea.php?nomArma=" . $inventario['nombre']."&&id=".$inventario["id"] ."'><input type='button' value='Elija arma' name='armasDefault'></a></td>";
+
         ?>
       </div>
       <?php
-      echo "</td>";
+
     echo "</tr>";
 
     }
+
+
     ?>
 
     </table>
+
 
 
 
@@ -64,4 +80,10 @@ class InventarioArmas{
 }
 
 
+
+$_SESSION['armaxdef'] = "<p class='error2'>Modificado con exito</p>";
+
+
  ?>
+</body>
+</html>
--- Tienda/wea.php
+++ Tienda/wea.php
@@ -3,20 +3,56 @@
 <?php
 session_start();
 include_once('connectBD.php');
-$nombre_arma = $_GET['inventarioNombre'];
+include 'inventarioUsuario.php';
+//$nombre_arma = $_GET['inventarioNombre'];
 
-
+/*
 $id_inventario = $_SESSION['id_inventario'];
 
 
     $Sentencia_sql="update inventario_armas set armasDefault=1 where inventario_id =".$id_inventario." and inventario_armas_nombre = '".$nombre_arma."';";
-    $Sentencia_sql2="update inventario_armas SET armasDefault=0 WHERE inventario_id =" .$id_inventario. " and armasDefault=1;";
+
     conectar($Sentencia_sql);
-        conectar($Sentencia_sql);
+
+//    $Sentencia_sql2="update inventario_armas SET armasDefault=0 WHERE inventario_id =" .$id_inventario. " and armasDefault=1;";
+  //      conectar($Sentencia_sql2);
     echo "cambiado con exito";
 
 
 
+*/
 
+$consulta=Consultar($_GET['nomArma'],$_GET['id']);
+
+function Consultar($nombre,$id){
+
+  $sql="select i.inventario_id as id,i.inventario_armas_nombre as nombre, i.armasDefault as arma, i.inventario_armas_velocidad, i.inventario_armas_fuerza, i.inventario_armas_agilidad, t.armas_imagen from
+  inventario_armas i, armas_tienda t where i.inventario_id =".$id." and i.inventario_armas_nombre='" . $nombre . "';";
+$result=conectar($sql);
+
+
+  $mostrar=mysqli_fetch_assoc($result);
+
+  return [
+//$inventario["inventario_armas_nombre"],$inventario["inventario_armas_velocidad"],$inventario["inventario_armas_fuerza"],$inventario["inventario_armas_agilidad"]
+$mostrar["nombre"],$mostrar["id"],$mostrar["arma"]
+  ];
+}
+
+//$modificar="update `juego`.`inventario_armas` SET `armasdefault` =0 WHERE (`inventario_id`=8) and (`inventario_armas_nombre` = 'Hacha');";
+  $modificar="update inventario_armas set armasdefault=1 where inventario_armas_nombre='" . $consulta[0] . "' and inventario_id=" . $consulta[1] . ";";
+  conectar($modificar);
+
+$modificar2="update inventario_armas set armasdefault=0 where inventario_armas_nombre!='" . $consulta[0] . "' and inventario_id=" . $consulta[1] . ";";
+conectar($modificar2);
+
+include_once 'inventarioUsuario.php';
+echo $_SESSION['armaxdef'];
+
+
+/*
+echo $consulta[1];
+echo $consulta[0];
+*/
 
 ?>
