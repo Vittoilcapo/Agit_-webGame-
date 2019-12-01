c0e348eb20fde8c9783aade6c7b96623ba2b41df
foloxx <fede10320@gmail.com>
2019-12-01 01:44:33 -0300

    Subir estadisticas terminado pero sin css correcto.
    Co-Authored-By: null <fvigliarolo@users.noreply.github.com>

--- Tienda/subirpuntos.php
+++ Tienda/subirpuntos.php
@@ -3,34 +3,50 @@ include_once("connectBD.php");
 include_once('usuario.php');
 session_start();
 
-
+$usuario_id = $_SESSION['ClaseUsuario']->usuario_id;
 $usuario_nombre = $_SESSION['ClaseUsuario']->usuario_nombre;
 $fuerza= $_SESSION['ClaseUsuario']->fuerza;
 $agilidad= $_SESSION['ClaseUsuario']->agilidad;
 $velocidad= $_SESSION['ClaseUsuario']->velocidad;
 
-$total= $fuerza + $agilidad + $velocidad;
-echo $total;
+$sql="select puntoDisponibles from estadisticas where usuario_id=$usuario_id;";
+$resultado=conectar($sql);
+
+while ($mostrar=mysqli_fetch_array($resultado)){
+  $disponiblesActual=$mostrar[0];
+
+echo "Total puntos para gastar: ";
+echo $disponiblesActual;
+echo "<br>";
+echo "<br>";
+}
 ?>
 
 <link rel="stylesheet" href="estilo_Inventario.css">
 <table class="grid-itemEstadisticas">
 <tr>
   <td>Velocidad</td>
-  <td>Fuerza</td>
+  <td>fuerza</td>
   <td>Agilidad</td>
 
 </tr>
+
 <?php
 echo "<tr>";
+  echo "<td>"; echo $velocidad; echo "</td>";
   echo "<td>"; echo $fuerza; echo "</td>";
   echo "<td>"; echo $agilidad; echo "</td>";
-  echo "<td>"; echo $velocidad; echo "</td>";
 echo "</tr>";
  ?>
-<form method="post" action="subirpuntos.php">
+<form method="post" action="validarsubirpuntos.php">
+<br>
+Fuerza
 <input type="text" name="ingresa_fuerza">
+<br>
+Velocidad
 <input type="text" name="ingresa_velocidad">
+<br>
+Agilidad
 <input type="text" name="ingresa_agilidad">
 <input type="submit" value="ingresar">
 
---
+++ Tienda/validarsubirpuntos.php
@@ -0,0 +1,65 @@
+<?php
+include_once('usuario.php');
+
+
+session_start();
+include_once("../Tienda/connectBD.php");
+
+$usuario_id = $_SESSION['ClaseUsuario']->usuario_id;
+
+
+$sql="select puntoDisponibles from estadisticas where usuario_id=$usuario_id;";
+$resultado=conectar($sql);
+
+while ($mostrar=mysqli_fetch_array($resultado)){
+  $disponiblesActual=$mostrar[0]; //Obtenemos la cantidad de puntos ganados por el usuario para invertir
+}
+
+//obtenemos los valores actuales de sus estadisticas
+$fuerzaActual= $_SESSION['ClaseUsuario']->fuerza;
+$agilidadActual= $_SESSION['ClaseUsuario']->agilidad;
+$velocidadActual= $_SESSION['ClaseUsuario']->velocidad;
+
+$totalActual= $fuerzaActual + $velocidadActual +$agilidadActual; //Guardamos el total en una variable, nos servira para descontar los puntos que invierte el usuario
+
+
+//obtenemos los valores que quiere aumentar
+                $_SESSION['fuerza']=$_REQUEST['ingresa_fuerza'];
+                $_SESSION['agilidad']=$_REQUEST['ingresa_agilidad'];
+                $_SESSION['velocidad']=$_REQUEST['ingresa_velocidad'];
+
+//guardamos los valores actuales mas los que el usuario se quiere aumentar
+                $fuerza=$_SESSION['fuerza'] + $fuerzaActual;
+                $agilidad=$_SESSION['agilidad'] + $agilidadActual;
+                $velocidad=$_SESSION['velocidad'] + $velocidadActual;
+
+//guardamos el total final de todos los puntos gastados y los que ya tenia
+$total= $fuerza + $agilidad + $velocidad;
+$disponibles= $disponiblesActual + $fuerzaActual + $agilidadActual+ $velocidadActual; //Verificamos cual es valor maximo que puede alcanzar las estadisticas  del usuario en base a la suma de sus estadisticas y sus puntos disponibles
+$resta=$total - $totalActual; //guardamos los puntos que gasta, para restarlos de los que ya tiene
+$quedan=$disponiblesActual - $resta; //guardamos la resta de los puntos que tiene menos los que gasto
+
+/*
+echo "puntos actuales: ".$disponiblesActual;
+echo "<br>";
+echo "diferencia: ".$resta;
+echo "<br>";
+echo "le quedaran: ".$quedan;
+*/
+
+if ($total > $disponibles){
+  echo "no te haga el langa perri que eso punto no los tene";
+}elseif ($total <= $disponibles){
+
+$sql1="update `juego`.`estadisticas` SET `estadisticas_velocidad`=$velocidad, `estadisticas_fuerza`=$fuerza, `estadisticas_agilidad`=$agilidad WHERE (`usuario_id` =$usuario_id );";
+$resultado= conectar($sql1);
+
+$sql3="update `juego`.`estadisticas` SET `puntoDisponibles` =$quedan WHERE (`usuario_id`=$usuario_id)";
+$resultado= conectar($sql3);
+}
+
+
+
+
+
+ ?>
